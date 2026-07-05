5f2a591d241d05e180e3f549725da0d78976ce73
theghosty99-lgtm <theghosty99@gmail.com>
2026-07-05 07:11:42 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
 BOT_NAME = "AntiScamGuardian"
-
 DATABASE = "database/guild.db"
-
 EMBED_COLOR = 0x2F3136
